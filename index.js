7eff8e5934d738dd4294bd611ab449dc74cbc72c
PZTV <omer.akkiyal@ogr.sakarya.edu.tr>
2026-01-31 00:12:18 +0300

    fix: remove hardcoded executablePath to use puppeteer's default browser cache

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: '/usr/bin/google-chrome-stable',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
